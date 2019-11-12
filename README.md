e1100e7076de255d9c5940c8918c3a2bef71aa73
José Manuel Moraleda <josemanuel.moraleda@gmail.com>
2019-11-12 07:53:20 +0100

    Sección 17 Terminada. Node + MySQL + TypeScript.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Notas:
+
+Aplicación de Node con Typescript y MySQL.
+
+Para compilar
+
+```
+tsc
+```
+
+Para compilar y reconstruir el directorio de 'dist'
+npm run build
+
+Para lanzar la aplicación:
+
+nodemon dist/index
---
+++ src/mysql/mysql.ts
@@ -0,0 +1,64 @@
+import mysql = require('mysql');
+
+
+export default class MySQL {
+
+    // Patrón singleton para evitar múltiples conexiones de la base de datos abiertas.
+    private static _instance: MySQL;
+
+    cnn: mysql.Connection;
+    conectado: boolean = false;
+
+    constructor(  ){
+        console.log('Clase inicializada');
+
+        this.cnn = mysql.createConnection({
+            host: 'localhost',
+            user: 'node_user',
+            password: '123456',
+            database: 'node_db'
+        });
+
+        this.conectarDB();
+
+    }
+
+
+    public static get instance(){
+        // Si existe la instancia la devolvemos y, si no, la creamos
+        return this._instance || ( this._instance = new this() );
+    }
+
+    static ejecutarQuery( query:string, callback: Function ){
+        this.instance.cnn.query( query, (err, results:Object[], fields) => {
+            if ( err ) {
+                console.log('Error en query.');
+                console.log( err );
+                return callback(err);
+            }
+
+            if ( results.length === 0) {
+                callback('El registro solicitado no existe.');
+            } else {
+                callback( null, results )
+            }
+
+        });
+    }
+
+
+    private conectarDB() {
+        this.cnn.connect( ( err: mysql.MysqlError ) => {
+           if ( err ) {
+               console.log(err.message);
+               return;
+           } 
+
+            this.conectado = true;
+            console.log('Base de datos online!');
+
+        });
+    }
+
+
+}
